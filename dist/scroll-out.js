421682d7f3539738c14f6ed6291248d58d7f862f
Christopher Wallis <christopher.j.wallis@gmail.com>
2018-06-28 22:56:40 -0400

    added intersect-x and intersect-y

--- dist/scroll-out.js
+++ dist/scroll-out.js
@@ -58,17 +58,6 @@ var ScrollOut = (function () {
     }
 
     /**
-     *
-     * @param {number} p0
-     * @param {number} s0
-     * @param {number} p1
-     * @param {number} s1
-     */
-    function getRatio(p0, s0, p1, s1) {
-        return (clamp(p0 + s0, p1, p1 + s1) - clamp(p0, p1, p1 + s1)) / s0;
-    }
-
-    /**
      * @param {number} x
      */
     function sign(x) {
@@ -179,9 +168,9 @@ var ScrollOut = (function () {
                 var w = el.clientWidth;
                 var h = el.clientHeight;
 
-                // find visible ratios for each element
-                var visibleX = getRatio(x, w, cx, cw);
-                var visibleY = getRatio(y, h, cy, ch);
+                // find visible ratios for each element 
+                var visibleX = (clamp(x + w, cx, cx + cw) - clamp(x, cx, cx + cw)) / w;
+                var visibleY = (clamp(y + h, cy, cy + ch) - clamp(y, cy, cy + ch)) / h; 
 
                 var ctx = {
                     visibleX: visibleX,
@@ -189,7 +178,9 @@ var ScrollOut = (function () {
                     offsetX: x,
                     offsetY: y,
                     elementWidth: w,
-                    elementHeight: h
+                    elementHeight: h,
+                    intersectX: visibleX == 1 ? 0 : sign(x - cx),
+                    intersectY: visibleY == 1 ? 0 : sign(y - cy)
                 };
 
                 // identify if this is visible "enough"
--- src/utils/math.js
+++ src/utils/math.js
@@ -9,17 +9,6 @@ export function clamp(v, min, max) {
 }
 
 /**
- *
- * @param {number} p0
- * @param {number} s0
- * @param {number} p1
- * @param {number} s1
- */
-export function getRatio(p0, s0, p1, s1) {
-    return (clamp(p0 + s0, p1, p1 + s1) - clamp(p0, p1, p1 + s1)) / s0;
-}
-
-/**
  * @param {number} x
  */
 export function sign(x) {

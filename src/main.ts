96f1ce4208b2bdbd8bf9dba86a95f30861e18a2f
Marius Hogas <mhogas@gmail.com>
2019-02-08 10:59:16 +0200

    added index to ctx object
    per https://github.com/scroll-out/scroll-out/pull/33

--- src/main.ts
+++ src/main.ts
@@ -138,8 +138,9 @@ export default function(opts: IScrollOutOptions) {
       // Handle JavaScript callbacks.
       if (changeAndDetect(el, '_SV', visible)) {
         setAttrs(el, {scroll: visible ? 'in' : 'out'});
-        onChange(el, ctx, doc, x);
-        (visible ? onShown : onHidden)(el, ctx, doc, x);
+        ctx.index = x;
+        onChange(el, ctx, doc);
+        (visible ? onShown : onHidden)(el, ctx, doc);
       }
 
       // if this is shown multiple times, keep it in the list
